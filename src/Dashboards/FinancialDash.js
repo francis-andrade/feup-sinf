9890fb6a2da61a29e8b82a21ce213dd717311092
Sérgio Salgado <sergiodias.17@hotmail.com>
2018-11-25 17:47:48 +0000

    added KPI component

--- src/Dashboards/FinancialDash.js
+++ src/Dashboards/FinancialDash.js
@@ -4,6 +4,7 @@ import '../App.css';
 import '../styles/Common.style.css';
 import CalendarSelector from '../components/CalendarSelector'
 import LineGraphComponent from '../components/LineGraphComponent';
+import KPIComponent from '../components/KPIComponent';
 
 class FinancialDash extends Component {
     constructor(props) {
@@ -36,6 +37,14 @@ class FinancialDash extends Component {
                     </Col>
                     <Col lg={{ size: 5 }}>
                         <LineGraphComponent />
+                    </Col>
+                </Row>
+                <Row style={{ 'marginTop':'5vh' }} >
+                    <Col lg={{ size: 5, offset: 1 }} className='columnStack'>
+                        <KPIComponent />
+                    </Col>
+                    <Col lg={{ size: 5 }}>
+                        <KPIComponent />
                     </Col>
                 </Row>
             </div>
--- src/components/KPIComponent.js
+++ src/components/KPIComponent.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { Row, Col, Container } from 'reactstrap';
+import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown } from 'react-icons/fa';
+
+import '../styles/KPIComponent.style.css';
+import '../styles/Common.style.css';
 
 class KPIComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            growing: false,
+        };
+    }
+
     render() {
         return(
-            <div></div>
+            <Container className='componentBackground'>
+                <Row>
+                    <Col>
+                        <span className='kpiValue'>teste {this.state.growing? <FaRegArrowAltCircleUp style={{ color: 'green' }} /> : <FaRegArrowAltCircleDown style={{ color: 'red' }} />}</span>
+                    </Col>
+                </Row>
+                <hr className='kpiHr'/>
+                <Row>
+                    <Col>
+                        <span className='kpiTitle'>Sales Value</span>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
