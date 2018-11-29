f23b88d43f80a916026c448d3278fd474d46b751
Francisco Lopes <ftlopes93@gmail.com>
2018-11-29 17:17:29 +0000

    Add 2 SAF-T GET requests, total balances and total sales

--- server/index.js
+++ server/index.js
@@ -10,24 +10,55 @@ const app = express();
 
 let parsedXML;
 
-// TODO: temporary test query for reading fields in SAF-T
-app.get('/api/testQuery', (req, res) => {
-    
-    let xmlFile = fs.readFileSync(path.join(__dirname + '/SAF-T/SAFT_DEMOSINF_01-01-2016_31-12-2016.xml'), 'utf8');
-    parsedXML = XmlReader.parseSync(xmlFile);
+// Sums the given balance nodes read from a SAF-T file
+function sumBalance(query) {
 
+    let sum = 0;
+    query.each(node => {
+        sum += parseFloat(node['children'][0]['value']);
+    });
+    
+    return sum;
+}
+
+// TODO: return the value, only console logging for now
+// TODO: fix start point of query to GeneralLedgerAccounts
+// Sums the General Ledger Accounts' balances
+app.get('/api/sumLedgerBalances', (req, res) => {
+
+    // Get whole document as xml-query object
     const xq = xmlQuery(parsedXML);
 
-    let query = xq.find('ClosingDebitBalance')
+    // Calc sum for each relevant balance
+    let openingDebitBalanceSum = sumBalance(xq.find('OpeningDebitBalance'));
+    let closingDebitBalanceSum = sumBalance(xq.find('ClosingDebitBalance'));
+    let openingCreditBalanceSum = sumBalance(xq.find('OpeningCreditBalance'));
+    let closingCreditBalanceSum = sumBalance(xq.find('ClosingCreditBalance'));
 
-    // Iterate over ClosingDebitBalance
-    let closingDebitSum = 0;
-    for (let o of query['ast']) {
-        closingDebitSum += parseInt(o['children'][0]['value']);
-        //console.log(o['children'][0]['value']);
-    }
-    console.log(closingDebitSum);
-    res.send(closingDebitSum + '');
+    console.log("openDebit: " + openingDebitBalanceSum);
+    console.log("closeDebit: " + closingDebitBalanceSum);
+    console.log("openCredit: " + openingCreditBalanceSum);
+    console.log("closeCredit: " + closingCreditBalanceSum);
+
+    // res.send(closingDebitSum + '');
+});
+
+// TODO: return the value, only console logging for now
+// Gets the debit and credit totals of the sales
+app.get('/api/salesTotals', (req, res) => {
+
+    // Get whole document as xml-query object
+    const xq = xmlQuery(parsedXML);
+
+    // Get sales totals
+    let salesInvoiceQuery = xq.find('SalesInvoices');
+    let salesDebitTotal = parseFloat(salesInvoiceQuery.children().find('TotalDebit').children().text());
+    let salesCreditTotal = parseFloat(salesInvoiceQuery.children().find('TotalCredit').children().text());
+
+    console.log("salesDebit: " + salesDebitTotal);
+    console.log("salesCredit: " + salesCreditTotal);
+
+    // res.send(closingDebitSum + '');
 });
 
 // Parse a SAF-T file read from the file system and store it
