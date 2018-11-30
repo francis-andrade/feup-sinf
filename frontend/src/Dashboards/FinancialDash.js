8978aa75eb3991670482389125b500bb2763e3c6
Sérgio Salgado <sergiodias.17@hotmail.com>
2018-11-30 02:43:24 +0000

    adds near final design for financial dash, improves kpi component and overall screen layout

--- frontend/src/Dashboards/FinancialDash.js
+++ frontend/src/Dashboards/FinancialDash.js
@@ -14,11 +14,11 @@ class FinancialDash extends Component {
 
         };
 
-        this.data = {
+        this.revenue = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
-                    label: 'My First dataset',
+                    label: 'Revenue',
                     fill: true,
                     lineTension: 0.1,
                     backgroundColor: 'rgba(75,192,192,0.4)',
@@ -36,7 +36,61 @@ class FinancialDash extends Component {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40]
+                    data: [1843, 1928, 2058, 2185, 1956, 1856, 2075]
+                }
+            ]
+        };
+
+        this.costs = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Costs',
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [1382, 1258, 1342, 1290, 1392, 1502, 1482]
+                }
+            ]
+        };
+
+        this.income = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Income',
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [1593, 1602, 1759, 1857, 1620, 1595, 1645]
                 }
             ]
         };
@@ -53,35 +107,82 @@ class FinancialDash extends Component {
     }
 
     render() {
-        return (
+        return(
             <div className='dashboardBackground'>
                 <Row>
                     <Col>
                         <CalendarSelector />
                     </Col>
                 </Row>
-                <Row style={{ 'marginTop': '5vh' }} >
-                    <Col lg={{ size: 5, offset: 1 }} className='columnStack'>
-                        <GraphComponent type={'pie'} data={this.data} height={200} title={'Graph Title'} />
+                <Row style={{ 'marginTop': '5vh' }}>
+                    <Col lg={{ size: 1 }} />
+                    <Col lg={{ size: 5 }} xl className='columnStack'>
+                        <KPIComponent title={'Total Income'} type={'money'} currentValue={1645} previousValue={1000}/>
                     </Col>
-                    <Col lg={{ size: 5 }}>
-                        <GraphComponent type={'line'} data={this.data} height={200} title={'Graph Title1'} />
+                    <Col lg={{ size: 5 }} xl className='columnStack'>
+                        <KPIComponent title={'Total Expenses'} type={'money'} currentValue={834} previousValue={1000}/>
+                    </Col>
+                    <Col lg={{ size: 1 }} className='d-xl-none'/>
+                    <Col lg={{ size: 1 }} className='d-xl-none'/>
+                    <Col lg={{ size: 5 }} xl className='columnStack'>
+                        <KPIComponent title={'Total Revenue'} type={'money'} currentValue={2075} previousValue={1000}/>
+                    </Col>
+                    <Col lg={{ size: 5 }} xl className='columnStack'>
+                        <KPIComponent title={'Total Asset Value'} type={'money'} currentValue={3966} previousValue={1000}/>
+                    </Col>
+                    <Col lg={{ size: 1 }} />
+                </Row>
+                <Row className='rowStack'>
+                    <Col xl={{ size: 6 }}>
+                        <Row>
+                            <Col lg={{ size: 1 }} xl={{ size: 2 }} />
+                            <Col lg className='columnStack'>
+                                <KPIComponent title={'Accounts Payable'} type={'money'} currentValue={857} previousValue={1000}/>
+                            </Col>
+                            <Col lg className='columnStack'>
+                                <KPIComponent title={'Accounts Receivable'} type={'money'} currentValue={1672} previousValue={1000}/>
+                            </Col>
+                            <Col lg={{ size: 1 }} className='d-xl-none' />
+                        </Row>
+                        <Row className='rowStack'>
+                            <Col lg={{ size: 1 }} xl={{ size: 2 }} />
+                            <Col lg className='columnStack'>
+                                <KPIComponent title={'Financial Autonomy'} type={'percentage'} currentValue={140} previousValue={126}/>
+                            </Col >
+                            <Col lg className='columnStack'>
+                                <KPIComponent title={'General Liquidity'} type={'percentage'} currentValue={106} previousValue={102}/>
+                            </Col>
+                            <Col lg={{ size: 1 }} className='d-xl-none' />
+                        </Row>
+                    </Col>
+                    <Col xl={{ size: 6 }}>
+                        <Row>
+                            <Col lg={{ size: 1 }} className='d-xl-none' />
+                            <Col className='columnStack'>
+                                <GraphComponent type={'line'} data={this.revenue} title={'Revenue History'} />
+                            </Col>
+                            <Col lg={{ size: 1 }} xl={{ size: 2 }} />
+                        </Row>
                     </Col>
                 </Row>
-                <Row style={{ 'marginTop': '5vh' }} >
-                    <Col lg={{ size: 5, offset: 1 }} className='columnStack'>
-                        <GraphComponent type={'line'} data={this.data} height={200} title={'Graph Title2'} />
+                <Row className='rowStack'>
+                    <Col xl={{ size: 6 }}>
+                        <Row>
+                            <Col lg={{ size: 1 }} xl={{ size: 2 }} />
+                            <Col className='columnStack'>
+                                <GraphComponent type={'line'} data={this.costs} title={'Costs History'} />
+                            </Col>
+                            <Col lg={{ size: 1 }} className='d-xl-none' />
+                        </Row>
                     </Col>
-                    <Col lg={{ size: 5 }}>
-                        <GraphComponent type={'line'} data={this.data} height={200} title={'Graph Title3'} />
-                    </Col>
-                </Row>
-                <Row style={{ 'marginTop': '5vh' }} >
-                    <Col lg={{ size: 5, offset: 1 }} className='columnStack'>
-                        <KPIComponent title={'KPI Title'} />
-                    </Col>
-                    <Col lg={{ size: 5 }}>
-                        <KPIComponent title={'KPI Title'} />
+                    <Col xl={{ size: 6 }}>
+                        <Row>
+                            <Col lg={{ size: 1 }} className='d-xl-none' />
+                            <Col className='lastElement'>
+                                <GraphComponent type={'line'} data={this.income} title={'Income History'} />
+                            </Col>
+                            <Col lg={{ size: 1 }} xl={{ size: 2 }} />
+                        </Row>
                     </Col>
                 </Row>
             </div>
--- frontend/src/components/KPIComponent.js
+++ frontend/src/components/KPIComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col, Container } from 'reactstrap';
-import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown } from 'react-icons/fa';
+import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown, FaRegArrowAltCircleRight } from 'react-icons/fa';
 
 import '../styles/KPIComponent.style.css';
 import '../styles/Common.style.css';
@@ -9,22 +9,36 @@ class KPIComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            growing: false,
+            
         };
     }
 
     render() {
+        let icon;
+        if (this.props.currentValue === this.props.previousValue) {
+            icon = <FaRegArrowAltCircleRight style={{ color: 'orange' }} />
+        } else if (this.props.currentValue > this.props.previousValue) {
+            icon = <FaRegArrowAltCircleUp style={{ color: 'green' }} />
+        } else if (this.props.currentValue < this.props.previousValue) {
+            icon = <FaRegArrowAltCircleDown style={{ color: 'red' }} />
+        }
+
         return(
-            <Container className='componentBackground'>
+            <Container className='kpiContainer componentBackground'>
                 <Row>
                     <Col>
-                        <span className='kpiValue'>teste {this.state.growing? <FaRegArrowAltCircleUp style={{ color: 'green' }} /> : <FaRegArrowAltCircleDown style={{ color: 'red' }} />}</span>
+                        <span className='kpiValue'>{this.props.type === 'money' ? '€' : ''} {this.props.currentValue}{this.props.type === 'money' ? 'K' : '%'} {icon}</span>
                     </Col>
                 </Row>
                 <hr className='kpiHr'/>
                 <Row>
                     <Col>
                         <span className='kpiTitle'>{this.props.title}</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <span className='kpiInfo'>{Number(((this.props.currentValue - this.props.previousValue) / this.props.previousValue * 100).toFixed(1))}% change from previous time frame</span>
                     </Col>
                 </Row>
             </Container>
