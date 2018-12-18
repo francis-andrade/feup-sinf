7b41530e793795607fbd0e2495541246dc4489b6
francis-andrade <up201503481@fe.up.pt>
2018-12-18 18:08:46 +0000

    added inventory value query

--- frontend/src/Dashboards/LogisticsDash.js
+++ frontend/src/Dashboards/LogisticsDash.js
@@ -97,7 +97,7 @@ class LogisticsDash extends Component {
           .then(response => response.json())
           .then(data => this.setState({ artigos: data }));*/
 
-    getInventory(){
+    getInventorySAFT(){
         fetch('http://localhost:5000/api/inventory',
         {     
 
@@ -107,8 +107,28 @@ class LogisticsDash extends Component {
         .then(response => response.json())
         .then(data =>{ 
             this.setState({ currentInventory: data.toFixed(0), currentInventoryLoading: false }); 
-            
-          } ); 
+          }); 
+    }
+
+    getInventory(){
+        fetch('http://localhost:2018/WebApi/Administrador/Consulta',
+          {     
+
+              method: "POST",
+              body: JSON.stringify("EXEC INV_EntradasSaidas @pCamposGrelha = 'NULL AS GROUP1,NULL AS GROUP2,NULL AS GROUP3,NULL AS GROUP4,NULL AS GROUP5,IdChave1 = NULL ,IdChave2 = NULL ,IdChave3 = CAST(IdChave3 AS NVARCHAR(36)),IdChave4 = NULL ,IdReserva,IdTipoOrigem = NULL ,Modulo = NULL ,Documento = NULL ,NomeApl = NULL ,TipoMovimento = NULL ,PeriodoMensal = NULL ,PeriodoTrimestral = NULL ,PeriodoSemestral = NULL ,HoraDoc = NULL ,DataDoc = NULL ,AnoDoc = NULL, Artigo,EstadoStock,ArtigoPai = NULL ,Descricao,DescArmazem = NULL ,DescFamilia = NULL ,DescSubFamilia = NULL ,DescMarca = NULL ,DescModelo = NULL ,Armazem = NULL ,Localizacao = NULL ,Lote = NULL ,Familia = NULL ,SubFamilia = NULL ,Marca = NULL ,Modelo = NULL ,Unidade = NULL ,NumeroSerie = NULL ,Quantidade = SUM(Quantidade),ValorMBase = SUM(ValorMBase),NumRegisto = NULL ,NULL AS DATAFILLCOL',@pCamposGroupBy = ' CAST(IdChave3 AS NVARCHAR(36)),IdReserva,Artigo,EstadoStock,Descricao',@pCasePeriodoMensal = ' CASE @Campo@ WHEN 1 THEN ''01-Janeiro''  WHEN 2 THEN ''02-Fevereiro''  WHEN 3 THEN ''03-Março''  WHEN 4 THEN ''04-Abril''  WHEN 5 THEN ''05-Maio''  WHEN 6 THEN ''06-Junho''  WHEN 7 THEN ''07-Julho''  WHEN 8 THEN ''08-Agosto''  WHEN 9 THEN ''09-Setembro''  WHEN 10 THEN ''10-Outubro''  WHEN 11 THEN ''11-Novembro''  WHEN 12 THEN ''12-Dezembro''  END ',@pCasePeriodoTrimestral = ' CASE WHEN @Campo@ >= 1 AND @Campo@ <= 3 THEN ''1º Trimestre''  WHEN @Campo@ >= 4 AND @Campo@ <= 6 THEN ''2º Trimestre''  WHEN @Campo@ >= 7 AND @Campo@ <= 9 THEN ''3º Trimestre''  WHEN @Campo@ >= 10 AND @Campo@ <= 12 THEN ''4º Trimestre''  END ',@pCasePeriodoSemestral = ' CASE WHEN @Campo@ >= 1 AND @Campo@ <= 6 THEN ''1º Semestre''  WHEN @Campo@ >= 7 AND @Campo@ <= 12 THEN ''2º Semestre''  END ',@pCaseModulo = 'CASE  WHEN @Campo@ = ''c8137b77-d1a4-4f6c-917e-0d111e7909fa'' THEN ''Inventário - Composições''  WHEN @Campo@ = ''53402401-3409-408a-8310-0f1661ebb4b5'' THEN ''Vendas''  WHEN @Campo@ = ''919da49a-49f4-475f-8d28-4a330f80af33'' THEN ''Produção''  WHEN @Campo@ = ''750a81d8-9bda-4842-86c6-53b27b48ff15'' THEN ''Internos''  WHEN @Campo@ = ''2daaccdf-f61f-4093-9b0d-6b717b299350'' THEN ''Inventário - Transferências''  WHEN @Campo@ = ''5424f810-cee8-4be7-863b-79ec7d6bcfdc'' THEN ''Inventário - Transferências Artigo''  WHEN @Campo@ = ''ce1bdb74-6287-4041-b77d-a84ea1518a04'' THEN ''Compras''  WHEN @Campo@ = ''c6374558-1ad3-4465-bad4-e9e2babd7806'' THEN ''Pagamentos e Recebimentos''  END ',@pCaseTipoMovimento = ' CASE WHEN @Campo@ = ''E'' THEN ''Entrada''  WHEN @Campo@ = ''S'' THEN ''Saída''  END ',@pDataInicial = '1/1/2018',@pDataFinal = '12/18/2018',@pFiltroDocumentos = '( 1 = 0  OR (docs.IdTipoOrigem IN (''c8137b77-d1a4-4f6c-917e-0d111e7909fa'') AND docs.Chave1 IN (''COM'', ''DEC'')) OR (docs.IdTipoOrigem IN (''53402401-3409-408a-8310-0f1661ebb4b5'') AND docs.Chave1 IN (''ECL'', ''FA'', ''FAI'', ''FI'', ''FM'', ''FO'', ''FR'', ''NC'', ''VD'')) OR (docs.IdTipoOrigem IN (''750a81d8-9bda-4842-86c6-53b27b48ff15'') AND docs.Chave1 IN (''AIN'', ''AIP'', ''ES'', ''ESI'', ''SS'', ''SSI'')) OR (docs.IdTipoOrigem IN (''2daaccdf-f61f-4093-9b0d-6b717b299350'') AND docs.Chave1 IN (''TALE'', ''TE'', ''TLT'', ''TNS'', ''TRA'', ''TS'')) OR (docs.IdTipoOrigem IN (''5424f810-cee8-4be7-863b-79ec7d6bcfdc'') AND docs.Chave1 IN (''TA'')) OR (docs.IdTipoOrigem IN (''ce1bdb74-6287-4041-b77d-a84ea1518a04'') AND docs.Chave1 IN (''DVF'', ''VFA'', ''VFI'', ''VFM'', ''VFO'', ''VFP'', ''VGR'', ''VNC'', ''VVD'')) OR (docs.IdTipoOrigem IN (''c6374558-1ad3-4465-bad4-e9e2babd7806'') AND docs.Chave1 IN (''416'', ''417'', ''418'', ''FAF'', ''ISA'', ''IVA'', ''IVR'', ''LTF'', ''NCF'', ''NDF'', ''NFA'', ''SEG'', ''VDF'', ''VEN'')))',@pFiltroAdicionais = Null,@pListaDimensoes = 1,@pListaNumSerie = 1,@pTipoMovimento = Null,@pFilial = '000'")
+              ,
+              headers: {
+                  'Authorization' : `Bearer ${this.state.authentication['access_token']}`,
+                  'Content-Type': 'application/json'
+              },             
+          })
+          .then(response => response.json())
+          .then(data =>{ 
+              this.setState({shipments: data["DataSet"]["Table"] }); 
+              this.updateShipmentValue(data["DataSet"]["Table"]); 
+              this.updateDeliveryStatus(data["DataSet"]["Table"]);
+              this.updateDeliveriesDestinations(data["DataSet"]["Table"]);
+            } ); 
     }
 
     getInventoryPeriod(){
@@ -144,6 +164,17 @@ class LogisticsDash extends Component {
               this.updateDeliveryStatus(data["DataSet"]["Table"]);
               this.updateDeliveriesDestinations(data["DataSet"]["Table"]);
             } ); 
+    }
+
+    updateInventory(table){
+        let inventoryValue = 0
+        let products = []
+        let quantity = []
+        for(let index =0; index < table.length; index++){
+            inventoryValue += table[index]['Quantidade']*table[index]['ValorMBase']
+            products.push(table[index]['Descricao'])
+            quantity.push(table[index]['ValorMBase'])
+        }
     }
 
     updateShipmentValue(table){
@@ -219,7 +250,7 @@ class LogisticsDash extends Component {
         })
         .then(response => response.json())
         .then(data => this.setState({ authentication: data }))
-        .then(this.getInventory.bind(this))
+        .then(this.getInventorySAFT.bind(this))
         .then(this.getInventoryPeriod.bind(this))
         .then(this.getShipments.bind(this));   
     }
